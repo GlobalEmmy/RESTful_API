307d78aecbed4679d97a1e7c7048ac64a36381d7
GlobalEmmy <monyem44@gmail.com>
2022-11-10 12:58:43 +0100

    Updated routes in app.js file

--- app.js
+++ app.js
@@ -1,64 +1,59 @@
 const express = require("express");
-const bodyParser =require("body-parser");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-mongoose.connect('mongodb://localhost:27017/skillDB');
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost:27017/skillDB");
 
 const skillSchema = {
-    language: String,
-    level: String
-}
-const Skill = mongoose.model('Skill', skillSchema);
+  language: String,
+  level: String,
+};
+const Skill = mongoose.model("Skill", skillSchema);
 
-const skill_1 = new Skill({ 
-    language: 'Nodejs',
-    level: "Intermediate" 
+const skill_1 = new Skill({
+  language: "Nodejs",
+  level: "Intermediate",
 });
 // skill_1.save()
 
-app.get ("/skill", (req, res) => {
+app
+  .route("/skill")
+  .get((req, res) => {
     Skill.find((err, foundSkill) => {
-        if (!err){
+      if (!err) {
         // console.log(foundSkill)
-        res.send(foundSkill)
-        }else {
-            res.send(err)
-        }
-       
-    })
-});
-
-app.post ("/skill", (req,res) => {
-
-    const newSkill = new Skill({ 
-        language: req.body.language,
-        level: req.body.level 
+        res.send(foundSkill);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newSkill = new Skill({
+      language: req.body.language,
+      level: req.body.level,
     });
     newSkill.save((err) => {
-        if (!err) {
-            res.send("Saved successfully")
-        } else {
-            res.send (err)
-        }
+      if (!err) {
+        res.send("Saved successfully");
+      } else {
+        res.send(err);
+      }
     });
-
-    app.delete ("/skill", (req, res) => {
-        Skill.deleteMany ((err) => {
-            if (!err) {
-                res.send("Deleted successfully")
-            } else {
-                res.send (err)
-            }
-        })
+  })
+  .delete((req, res) => {
+    Skill.deleteMany((err) => {
+      if (!err) {
+        res.send("Deleted successfully");
+      } else {
+        res.send(err);
+      }
     });
+  });
 
-
-    // console.log(req.body.language)
-    // console.log(req.body.level)
-})
- const PORT = process.env.PORT || 3000;
-app.listen (PORT, () => {
-    console.log(`Server running successfully on port: ${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running successfully on port: ${PORT}`);
+});
